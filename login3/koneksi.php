e13ea87d1dc6091f91528dac3d1e1fa076984aad
lululatifah27 <lululatifahh@gmail.com>
2025-04-29 09:47:21 +0700

    first commit

--- login3/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "dbdayeuhluur");
-if (mysqli_connect_errno()) {
-    echo "koneksi database gagal : " . mysqli_connect_error();
-}
-
-?>
--- register/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start(); //inisialisasi session
-    if(session_destroy()) {//menghapus session
-        header("Location: /WebdesDayeuhluhur1/register/login.php"); //jika berhasil maka akan diredirect ke file index.php
-    }
-?>
